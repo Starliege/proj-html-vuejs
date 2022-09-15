d839742c05031f5530e9cd67b3acccad217660a8
Starliege <urrianif@gmail.com>
2022-09-15 16:20:49 +0200

    Recent courses done

--- src/components/sections/RecentCourses.vue
+++ src/components/sections/RecentCourses.vue
@@ -1,6 +1,187 @@
 <template>
     <div class="container">
-
+        <h1 style="display: flex; justify-content: center;">Recent Courses</h1>
+        <ul>
+            <li><a href="">All categories</a></li>
+            <li><a href="">Business</a></li>
+            <li><a href="">Design</a></li>
+            <li><a href="">Development</a></li>
+            <li><a href="">IT & Software</a></li>
+            <li><a href="">Lifestyle</a></li>
+            <li><a href="">Marketing</a></li>
+            <li><a href="">Office Productivity</a></li>
+        </ul>
+        <div class="cardcontainer">
+            <div class="card">
+                <img src="../../assets/img/895786_7b4b_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">The Complete iOS 10 & Swift 3 Developer Course</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 120px; font-weight: bold">Free</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/246154_d8b0_3-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Web Design for Beginners: Real World Coding in HTML & CSS</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 100px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$129.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$65</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/752950_b773-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">GitHub Ultimate: Master Git and GitHub - Beginner to Expert</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$99.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$50</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/1253188_58f7_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">The Complete iOS 11 & Swift Developer Course - Build 20 Apps</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$199.99</span>
+                    <span style="margin-left: -30px; font-weight: bold; margin-top: 5px;">$100</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/919872_ed54_6-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Android Java MasterClass - Become an App Developer</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$99.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$50</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/951684_9c1a_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Xamarin Forms: Build Native Cross-platform Apps with C#</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$189.99</span>
+                    <span style="margin-left: -30px; font-weight: bold; margin-top: 5px;">$95</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/895786_7b4b_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">The Complete iOS 10 & Swift 3 Developer Course</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 120px; font-weight: bold">Free</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/246154_d8b0_3-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Web Design for Beginners: Real World Coding in HTML & CSS</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 100px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$129.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$65</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/752950_b773-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">GitHub Ultimate: Master Git and GitHub - Beginner to Expert</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$99.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$50</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/1253188_58f7_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">The Complete iOS 11 & Swift Developer Course - Build 20 Apps</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$199.99</span>
+                    <span style="margin-left: -30px; font-weight: bold; margin-top: 5px;">$100</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/919872_ed54_6-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Android Java MasterClass - Become an App Developer</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$99.99</span>
+                    <span style="margin-left: -25px; font-weight: bold; margin-top: 5px;">$50</span>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/img/951684_9c1a_2-272x161.jpg" alt="">
+                <span style="color: lightgray; font-size: 16px; padding-left: 20px; display: block; margin-bottom: 10px;">Development</span>
+                <div style="margin-left: 20px; margin-right: 20px; margin-bottom: 30px; height: 50px; border-bottom: 1px solid lightgray;">Xamarin Forms: Build Native Cross-platform Apps with C#</div>
+                <div style="display: flex; flex-direction: row; width: 20%; margin-left: 20px;">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/starfull.svg" alt="">
+                    <img style="height: 14px;" src="../../assets/img/icons/staremptyl.svg" alt="">
+                    <span style="margin-left: 110px; font-weight: bold; text-decoration: line-through; font-size: 13px; color: lightgray; margin-top: -10px">$189.99</span>
+                    <span style="margin-left: -30px; font-weight: bold; margin-top: 5px;">$95</span>
+                </div>
+            </div>
+        </div>
+        <div style="display: flex; justify-content: center;">
+            <a href="" style="padding-top: 30px;">Show all</a>
+        </div>
     </div>
 </template>
 
@@ -12,9 +193,46 @@
 
 <style lang="scss" scoped>
     .container {
-        height: 1260px;
+        height: 1150px;
         width: 93%;
         padding-left: 4%;
         padding-right: 3%;
+
+        h1 {
+            font-size: 48px;
+            margin-bottom: 50px;
+        }
+
+        ul {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 50px;
+
+            li {
+                display: inline;
+                margin-right: 30px;
+                font-size: 20px;
+            }
+        }
+
+        .cardcontainer {
+
+            display: flex;
+            flex-wrap: wrap;
+
+            .card {
+                width: 270px;
+                height: 360px;
+                border: 1px solid lightgray;
+                margin: 10px;
+                margin-bottom: 40px;
+
+                img {
+                    height: 50%;
+                    width: 100%;
+                    margin-bottom: 10px;
+                }
+            }
+        }
     }
 </style>
